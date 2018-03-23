5ad4ecd21ef59409029e6d314034a14e1b000d74
Anders Kalhauge <anders@kalhauge.dk>
2018-03-23 10:28:15 +0100

    Member model

--- Main.elm
+++ Main.elm
@@ -11,25 +11,40 @@ main =
     , subscriptions = subscriptions
     }
 
-type alias Model = String
+type alias Member =
+  { id : Int
+  , name : String
+  , email : String
+  }
+
+type alias Model = Member
 
 type Msg
-  = NoMessage
-  | SomeMessage
+  = NameChanged String
+  | EmailChanged String
+  | ButtonClicked
 
 init : (Model, Cmd Msg)
-init = ("No information yet", Cmd.none)
+-- init = (Member 7 "Anders And" "aa@andeby.quack", Cmd.none)
+init = ({ id = 7, name = "Anders And", email = "aa@andeby.quack"}, Cmd.none)
 
 view : Model -> Html Msg
 view model =
-  div [] [ text model ]
+  div []
+    [ text ("This is "++model.name++" with number "++(toString model.id))
+    , input [ onInput NameChanged, value model.name ] []
+    , input [ onInput EmailChanged, value model.email ] []
+    , button [ onClick ButtonClicked ] [ text "Click me!" ]
+    ]
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update message model =
   case message of
-    NoMessage ->
-      (model, Cmd.none)
-    SomeMessage ->
+    NameChanged newName ->
+      ( { model | name = newName }, Cmd.none)
+    EmailChanged newEmail ->
+      ( { model | email = newEmail }, Cmd.none)
+    ButtonClicked ->
       (model, Cmd.none)
 
 subscriptions : Model -> Sub Msg
